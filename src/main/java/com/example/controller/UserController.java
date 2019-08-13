b709732804fe4b61c95568cacf148ee05833a45a
liuan <864705455@qq.com>
2019-08-13 19:22:58 +0800

    v1.1 秒杀项目测试版

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -8,14 +8,15 @@ import com.example.service.ex.UserExistException;
 import com.example.service.ex.UserNotExistException;
 import com.example.service.ex.UserRegisterFailedException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.util.Date;
 
 @Controller
